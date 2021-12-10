070ca71b4cf657b2b4a90a18d4238b9a3b48fbdd
Paulo Salvatore <salvatoregames@gmail.com>
2021-12-10 09:54:10 -0300

    Endpoint de `Create` implementado

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 
+// Informamos ao Express que iremos utilizar JSON no Body
+app.use(express.json());
+
 app.get("/", function (req, res) {
     res.send("Hello, World!");
 });
@@ -26,6 +29,17 @@ app.get("/herois/:id", function (req, res) {
     const item = lista[id];
 
     res.send(item);
+});
+
+// [POST] "/herois" Create (Criar)
+app.post("/herois", function (req, res) {
+    // console.log(req.body, typeof req.body);
+
+    const item = req.body.nome;
+
+    lista.push(item);
+
+    res.send("Registro criado com sucesso!");
 });
 
 app.listen(3000);
